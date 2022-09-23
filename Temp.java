f9a6d24815d975b4f9e0e6e177b7ccb009c231c1
Amit Gupta <m9.a1.gamer@gmail.com>
2022-09-23 10:41:56 +0530

    modified:   Temp.java 	modified:   codechef.cpp 	modified:   codechef.exe 	modified:   input.txt web d codes

--- Temp.java
+++ Temp.java
@@ -1,15 +1,4 @@
-public class Temp {
-    public static void main(String args[]) {
-        int n = 4;
-        for (int i = n; i > 0; i--) {
-            for (int j = 1; j < n; j++) {
-                if (j >= i) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-    }
+class Btech{
+    int total_sub;
+    
 }
